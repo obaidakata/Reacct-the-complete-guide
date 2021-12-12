30b4f7601e39aaef84dbcd25eb20bec60dc69d3e
obaida kata <54075632+obaidakata@users.noreply.github.com>
2021-12-12 22:02:49 +0200

    react- group animation

--- 24-animating-ract-apps/src/App.js
+++ 24-animating-ract-apps/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Transition from 'react-transition-group/Transition';
+
 import './App.css';
 import Modal from './components/Modal/Modal';
 import Backdrop from './components/Backdrop/Backdrop';
@@ -7,7 +9,8 @@ import List from './components/List/List';
 
 class App extends Component {
 	state = {
-		modelIsOpen: false
+		modelIsOpen: false,
+		showBlock: false
 	};
 
 	showModel = () => {
@@ -22,8 +25,27 @@ class App extends Component {
 		return (
 			<div className="App">
 				<h1>React Animations</h1>
-				<Modal show={this.state.modelIsOpen} closed={this.closeModel}/>
-				<Backdrop show={this.state.modelIsOpen}/>
+				<button className="Button" onClick={() => this.setState(prevState => ( {
+					...prevState,
+					showBlock: !prevState.showBlock
+				} ))}>Toggle
+				</button>
+				<Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+					{state => (
+						<div
+							style={{
+								backgroundColor: 'red',
+								width: 100,
+								height: 100,
+								margin: 'auto',
+								translation: 'opacity 1s ease-out',
+								opacity: state === 'exiting' ? 0 : 1,
+							}}>
+						</div>
+					)}
+				</Transition>
+				<br/>
+				{this.state.modelIsOpen ? <Modal closed={this.closeModel}/> : <Backdrop/>}
 				<button className="Button" onClick={this.showModel}>Open Modal</button>
 				<h3>Animating Lists</h3>
 				<List/>
